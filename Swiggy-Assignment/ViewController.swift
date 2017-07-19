1aef960903de7cb0ac81c51277e4765f8b1d58ec
Pavan Goyal <pavan.goyal@timesinternet.in>
2017-07-19 18:29:12 +0530

    Initial Commit

---
+++ Swiggy-Assignment/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiggy-Assignment
+//
+//  Created by Pavan Goyal on 19/07/17.
+//  Copyright © 2017 Pavan Goyal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
